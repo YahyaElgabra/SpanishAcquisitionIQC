5f1cd55de6c22415daa4b732f0c769d22997d5dd
Kyle Willick <kylewillick@gmail.com>
2016-07-15 17:16:30 -0400

    Change live 2D measurement views to 2D view, and allow single measurement .csv export.

--- spacq/gui/display/plot/live/list_3d.py
+++ spacq/gui/display/plot/live/list_3d.py
@@ -1,25 +1,18 @@
 import logging
 log = logging.getLogger(__name__)
 
-import functools
 import numpy
 from pubsub import pub
-from threading import Lock
-from time import localtime
 import wx
-from wx.lib.agw import floatspin
-
-from spacq.interface.resources import AcquisitionThread
-from spacq.interface.units import Quantity
 
 from ....config.measurement import MeasurementConfigPanel
 from ....tool.box import Dialog, MessageDialog
 
 try:
-	from ..two_dimensional import TwoDimensionalPlot
+	from ..surface import SurfacePlot
 except ImportError as e:
 	plot_available = False
-	log.debug('Could not import TwoDimensionalPlot: {0}'.format(str(e)))
+	log.debug('Could not import SurfacePlot: {0}'.format(str(e)))
 else:
 	plot_available = True
 
@@ -35,9 +28,7 @@ class PlotSettings(object):
 
 	def __init__(self):
 		self.enabled = plot_available
-		self.update_x = True
-		self.update_y = True
-		self.delay = Quantity(0.2, 's')
+		self.num_lines = 100
 
 
 class PlotSettingsDialog(Dialog):
@@ -65,11 +56,11 @@ class PlotSettingsDialog(Dialog):
 		capture_box.Add(capture_sizer, flag=wx.CENTER)
 		dialog_box.Add(capture_box, flag=wx.EXPAND|wx.ALL, border=5)
 
-		## Delay.
-		capture_sizer.Add(wx.StaticText(self, label='Delay (s):'),
+		## Number of lines.
+		capture_sizer.Add(wx.StaticText(self, label='Lines:'),
 				flag=wx.ALIGN_CENTER_VERTICAL|wx.ALIGN_RIGHT)
-		self.delay_input = floatspin.FloatSpin(self, min_val=0.2, max_val=1e4, increment=0.1, digits=2)
-		capture_sizer.Add(self.delay_input, flag=wx.CENTER)
+		self.lines_input = wx.SpinCtrl(self, min=2, max=1e4, initial=100)
+		capture_sizer.Add(self.lines_input, flag=wx.CENTER)
 
 		# End buttons.
 		button_box = wx.BoxSizer(wx.HORIZONTAL)
@@ -92,13 +83,13 @@ class PlotSettingsDialog(Dialog):
 	def GetValue(self):
 		plot_settings = PlotSettings()
 		plot_settings.enabled = self.enabled_checkbox.Value
-		plot_settings.delay = Quantity(self.delay_input.GetValue(), 's')
+		plot_settings.num_lines = self.lines_input.Value
 
 		return plot_settings
 
 	def SetValue(self, plot_settings):
 		self.enabled_checkbox.Value = plot_settings.enabled
-		self.delay_input.SetValue(plot_settings.delay.value)
+		self.lines_input.Value = plot_settings.num_lines
 
 
 class ListLiveViewPanel(wx.Panel):
@@ -114,33 +105,24 @@ class ListLiveViewPanel(wx.Panel):
 
 		# Defaults.
 		self.plot_settings = PlotSettings()
-		self.unit_conversion = 0
 
 		self.enabled = True
 		self.capturing_data = False
-		self.restart_live_view = False
 		self.resource_backup = None
-
-		# csv save file information
-		self.save_path = ''
-		self.defaultName=''
 
 		# Initialize recorded values.
 		self.init_values()
-
-		# This lock blocks the acquisition thread from acquiring.
-		self.running_lock = Lock()
 
 		# Plot and toolbar.
 		display_box = wx.BoxSizer(wx.VERTICAL)
 
 		## Plot.
 		if plot_available:
-			self.plot = TwoDimensionalPlot(self, color='blue')
+			self.plot = SurfacePlot(self, style='waveform')
 			display_box.Add(self.plot.control, proportion=1, flag=wx.EXPAND)
 
-#			self.plot.x_label = 'Waveform time (s)'
-#			self.plot.y_label = 'History'
+			self.plot.x_label = 'Waveform time (s)'
+			self.plot.y_label = 'History'
 		else:
 			display_box.Add((500, -1), proportion=1, flag=wx.EXPAND)
 
@@ -149,27 +131,10 @@ class ListLiveViewPanel(wx.Panel):
 			controls_box = wx.BoxSizer(wx.HORIZONTAL)
 			display_box.Add(controls_box, flag=wx.CENTER|wx.ALL, border=5)
 
-			### Manual data export
-			saveData_static_box = wx.StaticBox(self, label='Last trace')
-			saveData_box = wx.StaticBoxSizer(saveData_static_box, wx.HORIZONTAL)
-			controls_box.Add(saveData_box, flag=wx.CENTER)
-
-			self.csv_button = wx.Button(self, label='Save to .csv')
-			self.Bind(wx.EVT_BUTTON, self.onSave, self.csv_button)
-			saveData_box.Add(self.csv_button, flag=wx.CENTER)
-
 			### Capture.
 			capture_static_box = wx.StaticBox(self, label='Control')
 			capture_box = wx.StaticBoxSizer(capture_static_box)
 			controls_box.Add(capture_box, flag=wx.CENTER)
-
-			self.run_button = wx.Button(self, label='Run')
-			self.Bind(wx.EVT_BUTTON, self.OnRun, self.run_button)
-			capture_box.Add(self.run_button, flag=wx.CENTER)
-
-			self.pause_button = wx.Button(self, label='Pause')
-			self.Bind(wx.EVT_BUTTON, self.OnPause, self.pause_button)
-			capture_box.Add(self.pause_button, flag=wx.CENTER)
 
 			self.reset_button = wx.Button(self, label='Reset')
 			self.Bind(wx.EVT_BUTTON, self.OnReset, self.reset_button)
@@ -186,52 +151,10 @@ class ListLiveViewPanel(wx.Panel):
 
 		self.SetSizer(display_box)
 
-		# Acquisition thread.
-		callback = functools.partial(wx.CallAfter, self.add_values)
-		self.acq_thread = AcquisitionThread(self.plot_settings.delay, callback,
-				running_lock=self.running_lock)
-		self.acq_thread.daemon = True
-		self.acq_thread.start()
-
-		# Wait for a resource to begin capturing.
-		self.OnPause()
-		self.run_button.Disable()
-
 		# Subscriptions.
-		pub.subscribe(self.msg_resource, 'resource.added')
-		pub.subscribe(self.msg_resource, 'resource.removed')
 		pub.subscribe(self.msg_data_capture_start, 'data_capture.start')
 		pub.subscribe(self.msg_data_capture_data, 'data_capture.data')
 		pub.subscribe(self.msg_data_capture_stop, 'data_capture.stop')
-
-	@property
-	def running(self):
-		return self.pause_button.Enabled
-
-	@property
-	def resource(self):
-		return self.acq_thread.resource
-
-	@resource.setter
-	def resource(self, value):
-		# Ignore unreadable resources.
-		if value is not None and not value.readable:
-			value = None
-
-		if self.running:
-			# Currently running.
-			running = True
-			self.OnPause()
-		else:
-			running = False
-
-		self.acq_thread.resource = value
-
-		self.run_button.Enable(value is not None)
-
-		# Resume if applicable.
-		if running:
-			self.OnRun()
 
 	@property
 	def measurement_resource_name(self):
@@ -257,8 +180,8 @@ class ListLiveViewPanel(wx.Panel):
 		Clear captured values.
 		"""
 
-		self._times = numpy.array([])
-		self._values = numpy.array([])
+		self._lines = None
+		self.time_range = (0.0, 0.0)
 
 	def update_plot(self):
 		"""
@@ -266,19 +189,12 @@ class ListLiveViewPanel(wx.Panel):
 		"""
 
 		# Wait for at least one line.
-		if not len(self._times) > 0:
-			display_time = [0]
-			display_values = [0]
+		if self._lines is None:
+			self.plot.surface_data = None
 		else:
-			display_time = self._times
-			display_values = self._values
+			self.plot.surface_data = (self._lines, self.time_range, (1, len(self._lines)))
 
-		if self.plot_settings.update_x:
-			self.plot.x_autoscale()
-		if self.plot_settings.update_y:
-			self.plot.y_autoscale()
-
-		self.plot.x_data, self.plot.y_data = display_time, display_values
+		wx.CallAfter(self.plot.redraw)
 
 	def add_values(self, values):
 		"""
@@ -290,10 +206,27 @@ class ListLiveViewPanel(wx.Panel):
 
 		# Extract the times and the data values.
 		times, values = zip(*values)
+		time_range = min(times), max(times)
+
+		# Sanity check, since the new values must match existing ones.
+		if self._lines is not None:
+			if len(self._lines[-1]) != len(values):
+				log.warning('Data length mismatch: was {0}, became {1}'.format(len(self._lines[-1]), len(values)))
+				self.init_values()
+			elif self.time_range != time_range:
+				log.warning('Time range mismatch: was {0}, became {1}'.format(self.time_range, time_range))
+				self.init_values()
 
 		# Update values.
-		self._values = numpy.append(numpy.array([]), values)
-		self._times = numpy.append(numpy.array([]),times)
+		if self._lines is None:
+			self._lines = numpy.array([values])
+			self.time_range = time_range
+		else:
+			self._lines = numpy.append(self._lines, [values], 0)
+
+		cut_idx = len(self._lines) - self.plot_settings.num_lines
+		if cut_idx > 0:
+			self._lines = self._lines[cut_idx:]
 
 		# Plot.
 		self.update_plot()
@@ -304,61 +237,9 @@ class ListLiveViewPanel(wx.Panel):
 		"""
 
 		# Unsubscriptions.
-		pub.unsubscribe(self.msg_resource, 'resource.added')
-		pub.unsubscribe(self.msg_resource, 'resource.removed')
 		pub.unsubscribe(self.msg_data_capture_start, 'data_capture.start')
 		pub.unsubscribe(self.msg_data_capture_data, 'data_capture.data')
 		pub.unsubscribe(self.msg_data_capture_stop, 'data_capture.stop')
-
-		# Ensure the thread exits.
-		self.acq_thread.resource = None
-		self.acq_thread.done = True
-		if not self.running:
-			self.running_lock.release()
-		self.acq_thread.join()
-		del self.acq_thread
-
-	def onSave(self, evt=None):
-		"""
-		save the latest trace to a manually named csv
-		"""
-		outArray = numpy.column_stack((self._times,self._values))
-		
-		self.defaultName = 'listTrace_{0:04}-{1:02}-{2:02}_{3:02}-{4:02}-{5:02}.csv'.format(*localtime())
-		fdlg = wx.FileDialog(self, "Save last trace", "", self.defaultName, "CSV files(*.csv)|*.*", wx.FD_SAVE | wx.FD_OVERWRITE_PROMPT)
-		if fdlg.ShowModal() == wx.ID_OK:
-			self.save_path = fdlg.GetPath()
-			if not(self.save_path.endswith(".csv")):
-				self.save_path = self.save_path + ".csv"
-			numpy.savetxt(self.save_path,outArray,delimiter=',')
-
-	def OnRun(self, evt=None):
-		"""
-		Let the acquisition thread run.
-		"""
-
-		self.run_button.Disable()
-
-		if self.acq_thread.resource is None:
-			return
-
-		self.running_lock.release()
-
-		self.pause_button.Enable()
-
-	def OnPause(self, evt=None):
-		"""
-		Block the acquisition thread.
-		"""
-
-		if not self.running:
-			return
-
-		self.running_lock.acquire()
-
-		if self.acq_thread.resource is not None:
-			self.run_button.Enable()
-		self.pause_button.Disable()
 
 	def OnReset(self, evt=None):
 		self.init_values()
@@ -375,10 +256,6 @@ class ListLiveViewPanel(wx.Panel):
 		dlg = PlotSettingsDialog(self, ok_callback)
 		dlg.SetValue(self.plot_settings)
 		dlg.Show()
-
-	def msg_resource(self, name, value=None):
-		if self.measurement_resource_name is not None and name == self.measurement_resource_name:
-			self.resource = value
 
 	def msg_data_capture_start(self, name):
 		if name == self.measurement_resource_name:
@@ -409,7 +286,6 @@ class ListMeasurementFrame(wx.Frame):
 
 		## Live view.
 		self.live_view_panel = ListLiveViewPanel(self, global_store)
-		self.live_view_panel.SetMinSize((-1, 400))
 		frame_box.Add(self.live_view_panel, proportion=1, flag=wx.EXPAND)
 
 		self.SetSizerAndFit(frame_box)

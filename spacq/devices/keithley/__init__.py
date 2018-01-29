cbb0462e8a567e1de83c4769ef06971bdbc0e2f6
Kyle Willick <kylewillick@gmail.com>
2018-01-29 14:29:06 -0500

    Add basic Keithley sourceMeter 2450 implementation

--- spacq/devices/keithley/__init__.py
+++ spacq/devices/keithley/__init__.py
@@ -4,10 +4,10 @@ log = logging.getLogger(__name__)
 
 name = 'Keithley'
 
-from . import voltagesource230
-models = [voltagesource230]
+from . import voltagesource230, sourceMeter2450
+models = [voltagesource230, sourceMeter2450]
 log.debug('Found models for "{0}": {1}'.format(name, ''.join(str(x) for x in models)))
 
-from .mock import mock_voltagesource230
-mock_models = [mock_voltagesource230]
+from .mock import mock_voltagesource230, mock_sourceMeter2450
+mock_models = [mock_voltagesource230, mock_sourceMeter2450]
 log.debug('Found mock models for "{0}": {1}'.format(name, ''.join(str(x) for x in mock_models)))

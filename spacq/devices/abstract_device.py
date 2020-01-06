40a439b8c2b806bee12221a3152dafbc63d49462
stephen-laptop <stephenharrigan2@gmail.com>
2020-01-06 15:26:17 -0500

    remove try except block since it hid the error

--- spacq/devices/abstract_device.py
+++ spacq/devices/abstract_device.py
@@ -241,10 +241,7 @@ class AbstractDevice(SuperDevice):
 			except visa.VisaIOError as e:
 				raise DeviceNotFoundError('Could not open device at "{0}".'.format(self.connection_resource), e)
 		elif self.driver == drivers.telnet:
-			try:
-				self.device = telnetlib.Telnet(**self.connection_resource)
-			except Exception as e:
-				raise DeviceNotFoundError('Could not open device at "{0}".'.format(self.connection_resource), e)
+			self.device = telnetlib.Telnet(**self.connection_resource)
 
 		elif self.driver == drivers.lgpib:
 			try:

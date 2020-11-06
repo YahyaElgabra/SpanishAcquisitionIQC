345f58f367c5aadec513ac3275957cead32f9194
Andres Lombo <esteban.lombobecerra@gmail.com>
2020-11-06 11:21:42 -0500

    Changed data set

--- sweep_algorithm.py
+++ sweep_algorithm.py
@@ -32,17 +32,28 @@ from scipy.optimize import curve_fit
 current_dict = {}
 history = []
 
+Vrf_array = []
+Vdc_array = []
+
 # Create dictionary for curent value lookup
 # Note all keys are values with 3 decimal places to avoid rounding issues
-with open('SEP_tuning_files/pumping_data.csv','r') as file:               
+with open('SEP_tuning_files/2018-11-23_17-00-39.csv','r') as file:               
     reader = csv.reader(file)
     line_count = 0
     for row in reader:
         if line_count != 0:
-            key1 = round(float(row[7])/10,3)                                    # as Vrf data is x10 amplified
-            key2 = float(row[6])                                                # as Vdc data is ___ amplified
-            current_dict[(key1,key2)] = float(row[8])
+            key1 = round(float(row[2])/10,3)                                    # as Vrf data is x10 amplified
+            key2 = round(float(row[1])/10,3)                                    # as Vdc data is x10 amplified
+            current_dict[(key1,key2)] = float(row[3])
+
+            if key1 not in Vrf_array:
+                Vrf_array.append(key1)
+            if key2 not in Vdc_array:
+                Vdc_array.append(key2)
+
+            #print (key1,key2),float(row[3])
         line_count += 1
+
 
 def get_current(Vrf, Vdc):
 
@@ -156,7 +167,7 @@ def perform_fit(xdata,ydata,initial,n):
         error = sum([ abs(ydata[i] - f(xdata[i],p[0],p[1],p[2])) for i in range(len(xdata)) ])
     except:
         print "ERROR: RuntimeError: Optimal parameters not found: Number of calls to function has reached maxfev"
-        return 0, 0, [0,0,0,0], [None]
+        return 0, 0, [0,0,0,0], [0]
     return abs(delta), error, p, pcov
 
 def find_plateau(Vrf_array,Vdc_array,scan_range,plateau_tol,epsilon,frequency,fit_error):
@@ -185,18 +196,18 @@ def find_plateau(Vrf_array,Vdc_array,scan_range,plateau_tol,epsilon,frequency,fi
     # Turn on RF component of Vrf
     dummy = turn_on_rf_component_of_Vrf(0)
 
-    for Vdc in Vdc_array:
+    for Vrf in Vrf_array:                       #--------------- temporary switch ------------------
 
-        # Fix Vdc
-        dummy = set_Vdc(Vdc)
+        # Fix Vrf
+        dummy = set_Vrf(Vrf)                    # temporary switch
 
         x = []
         y = []
 
-        for Vrf in Vrf_array:
-            dummy = set_Vrf(Vrf)
-            x.append(Vrf)
-            y.append(get_current(Vrf,Vdc))
+        for Vdc in Vdc_array:                   # temporary switch
+            dummy = set_Vdc(Vdc)                # temporary switch
+            x.append(Vdc)                       # temporary switch
+            y.append(get_current(Vrf,Vdc))      
             if len(x) == scan_range:
                 for n in range(1,5):
                     global N
@@ -220,12 +231,23 @@ def find_plateau(Vrf_array,Vdc_array,scan_range,plateau_tol,epsilon,frequency,fi
     return None
 
 frequency = 100e6
-Vrf_array = [float(x)/1000 for x in range(710,801)] 
-Vdc_array = [0.001, 0.01, 0.02, 0.04, 0.05]
+#Vrf_array =  [round(0.76+i*(0.007/3),3) for i in range(211)]                 # for older data: [float(x)/1000 for x in range(710,801)] 
+#Vdc_array =  [round(0.675+i*(0.0245/17),3) for i in range(171)]              # for older data: [0.001, 0.01, 0.02, 0.04, 0.05]
+Vrf_array.sort()
+Vdc_array.sort()
 scan_range = 17
 plateau_tol = 0.015
 epsilon = 0.1
 fit_error = 0.03
+
+"""
+print '=================================================='
+count = 1
+for j in Vdc_array:
+    for i in Vrf_array:
+        print str(count)+':',(i,j), get_current(i,j)
+        count = count + 1
+"""
 
 x, y, delta, error = find_plateau(Vrf_array,Vdc_array,scan_range,plateau_tol,epsilon,frequency,fit_error)
 

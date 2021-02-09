f5d9ffdc72158dc47b54a7238331c22f548dc8eb
bleutooth65 <stephenharrigan2@gmail.com>
2021-02-09 11:00:43 -0500

    Implemented all useful functionality into devices

--- spacq/devices/pynq/fpga.py
+++ spacq/devices/pynq/fpga.py
@@ -30,7 +30,6 @@ class DACPort(AbstractSubdevice):
 			self.resources[name] = Resource(self, name, name)
 
 		self.resources['voltage'].units = 'V'
-		self.resources['span'].units = 'V'
 
 	@Synchronized()
 	def _connected(self):
@@ -41,7 +40,8 @@ class DACPort(AbstractSubdevice):
 			Warning("Device not connected!")
 
 		self.currentVoltage = 0
-		self.currentSpan = 5
+		self.currentSpan = '5V'
+		self.span_limits = [0, 5]
 
 	def __init__(self, device, num, *args, **kwargs):
 		"""
@@ -63,6 +63,8 @@ class DACPort(AbstractSubdevice):
 		"""
 		Set the voltage on this port, as a quantity in V.
 		"""
+		if value < self.span_limits[0] or value > self.span_limits:
+			raise ValueError("Voltage {} is out of the allowed span {1}".format(value, self.span_limits))
 		r = self.device.ask_raw(':5000/dac1?channel={0}&value={1}'.format(self.num, value))
 		if r.status_code != 200:
 			Warning("Voltage not properly set!") 
@@ -70,21 +72,32 @@ class DACPort(AbstractSubdevice):
 		self.currentVoltage = value
 
 	@property
-	@quantity_wrapped('V')
 	def span(self):
 		return self.currentSpan
 		
 	@span.setter
-	@quantity_unwrapped('V')
 	def span(self, value):
 		"""
 		Set the span on this port, as a quantity in V.
 		"""
-		r = self.device.ask_raw(':5000/dac1?channel={0}&span={1}V'.format(self.num, value))
+		possible_spans = ['5','10','pm2.5','pm5','pm10']
+
+		if value not in possible_spans:
+			raise ValueError('Span {0} not part of the allowed spans: {1}'.format(value, possible_spans))
+
+		# Deal with plus/minus limits differently than positive limits only
+		if value[0:1] == 'pm':
+			_value = '±' + value[2:]
+			self.span_limits = [-float(value), float(value)]
+		else:
+			_value = value
+			self.span_limits = [0, float(value)]
+
+		r = self.device.ask_raw(':5000/dac1?channel={0}&span={1}'.format(self.num, _value))
 		if r.status_code != 200:
 			Warning("Voltage not properly set!") 
 
-		self.currentSpan= value
+		self.currentSpan = value
 		
 class ADCPort(AbstractSubdevice):
 	'''
@@ -95,7 +108,7 @@ class ADCPort(AbstractSubdevice):
 		AbstractSubdevice._setup(self)
 
 		# Resources.
-		read_only = ['reading']
+		read_only = ['reading', 'filter']
 		for name in read_only:
 			self.resources[name] = Resource(self, name)
 
@@ -105,9 +118,10 @@ class ADCPort(AbstractSubdevice):
 	def _connected(self):
 		AbstractSubdevice._connected(self)
 		# Turn on port
-		r = self.device.ask_raw(':5000/adc2?channel={0}&on=true&inputpair=vin{1}vincom&filter=sinc5'.format(self.num, self.num)) # Hardcoding configuration and filter
+		r = self.device.ask_raw(':5000/adc2?channel={0}&on=true&inputpair=vin{1}vincom&filter=default'.format(self.num, self.num)) # Hardcoding configuration
 		if r.status_code != 200:
 			Warning("Device not connected!")
+		self.currentFilter = 'default'
 
 	def __init__(self, device, num, *args, **kwargs):
 		"""
@@ -147,6 +161,23 @@ class ADCPort(AbstractSubdevice):
 
 		return result
 
+	@property
+	def filter(self):
+		return self.currentFilter
+	
+	@filter.setter
+	def filter(self, value):
+		possible_filters = ['default', 'sin3', 'sin5']
+
+		if value not in possible_filters:
+			raise ValueError('Filter {0} not part of the allowed powers: {1}'.format(value, possible_filters))
+
+		r = self.device.ask_raw(':5000/adc2?filter={0}'.format(value))
+		if r.status_code != 200:
+			Warning("Filter not properly set!") 
+
+		self.currentFilter = value
+
 class OSCPort(AbstractSubdevice):
 	'''
 	An output port of the oscillator connected to the FPGA
@@ -156,22 +187,24 @@ class OSCPort(AbstractSubdevice):
 
 		# Resources.
 
-		read_write = ['frequency', 'power']
+		read_write = ['frequency', 'freq_resolution', 'power', 'power_on']
 		for name in read_write:
 			self.resources[name] = Resource(self, name, name)
 
 		self.resources['frequency'].units = 'Hz'
-		self.resources['power'].units = 'V'
+		self.resources['freq_resolution'].units = 'Hz'
 	
 	@Synchronized()
 	def _connected(self):
 		AbstractSubdevice._connected(self)
 		# Turn on port
-		r = self.device.ask_raw(':5000/osc1?channel={0}&on=true'.format(self.num))
+		r = self.device.ask_raw(':5000/osc1?channel={0}&on=true&freq=500e6&power=0&freq_resolution=100e3&power_down=true'.format(self.num))
 		if r.status_code != 200:
 			Warning("Device not connected!")
-		self.frequency = Quantity(500000000, 'Hz')
-		self.power = Quantity(0, 'V')
+		self.currentFrequency = 500e6
+		self.freq_resolution = 100e3
+		self.currentPower = 0
+		self.power_on = 'false'
 
 	def __init__(self, device, num, *args, **kwargs):
 		"""
@@ -205,16 +238,27 @@ class OSCPort(AbstractSubdevice):
 		self.currentFrequency = value
 
 	@property
-	@quantity_wrapped('V')
+	@quantity_wrapped('Hz')
+	def freq_resolution(self):
+		return self.freq_resolution
+
+	@freq_resolution.setter
+	@quantity_unwrapped('Hz')
+	def freq_resolution(self, value):
+		r = self.device.ask_raw(':5000/osc1?freq_resolution={0}'.format(value))
+		if r.status_code != 200:
+			Warning("freq_resolution not properly set!")
+
+		self.freq_resolution = value
+
+	@property
 	def power(self):
 		# The power output by the oscillator
-		# Currently arbitrary power levels, as Spanish Acquisition doesn't handle dBm currently (TODO: add dBm units)
 		return self.currentPower
 		
 	@power.setter
-	@quantity_wrapped('V')
-	def power(self,value):
-		possible_powers = [0,1,2,3]
+	def power(self, value):
+		possible_powers = ['0','1','2','3'] # corresponds to -5, -2, 1 or 4 dBm, respectively 
 
 		if value not in possible_powers:
 			raise ValueError('Value {0} not part of the allowed powers: {1}'.format(value, possible_powers))
@@ -223,7 +267,26 @@ class OSCPort(AbstractSubdevice):
 		if r.status_code != 200:
 			Warning("Voltage not properly set!") 
 
-		self.currentpower = value
+		self.currentPower = value
+
+	@property
+	def power_on(self):
+		return self.power_on
+
+	@power_on.setter
+	def power_on(self, value):
+		# Swap to match hardware code
+		possible_values = {'true':'false', 'false':'true'}
+		# Make robust to capitalization
+		value = value.lower()
+		if value not in possible_values:
+			raise ValueError('Value {0} not part of the allowed values: {1}'.format(value, possible_values.values()))
+
+		r = self.device.ask_raw(':5000/osc1?power_down={0}'.format(possible_values[value]))
+		if r.status_code != 200:
+			Warning("Voltage not properly set!")
+
+		self.power_on = value 
 
 class fpga(AbstractDevice):
 	"""
@@ -245,9 +308,9 @@ class fpga(AbstractDevice):
 			self.ADCports.append(port)
 			self.subdevices['ADCport{0}'.format(num)] = port
 
-		# port = OSCPort(self, 0, **self.port_settings)
-		# self.OSCports = [port]
-		# self.subdevices['OSCport{0}'.format(num)] = port
+		port = OSCPort(self, 0, **self.port_settings)
+		self.OSCports = [port]
+		self.subdevices['OSCport{0}'.format(num)] = port
 
 	def __init__(self, port_settings=None, *args, **kwargs):
 		"""
@@ -266,8 +329,6 @@ class fpga(AbstractDevice):
 	def _connected(self):
 		AbstractDevice._connected(self)
 		# Add any initialization here. Careful: if you reconnect it will run this
-
-
 
 name = 'Pynq FPGA board'
 implementation = fpga

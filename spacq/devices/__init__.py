6bfe0b64e84dc30225317ee8d7a2945d910613c6
zmerino <31289560+zmerino@users.noreply.github.com>
2022-03-17 12:29:55 -0400

    Delete spacq directory

--- spacq/devices/__init__.py
+++
@@ -1,2 +0,0 @@
-from . import agilent, iqc, oxford, rohde_schwarz, tektronix, keithley, cryomagnetics, lakeshore, stanford_research_systems, basel
-manufacturers = [agilent, iqc, oxford, rohde_schwarz, tektronix, keithley, cryomagnetics, lakeshore, stanford_research_systems, basel]

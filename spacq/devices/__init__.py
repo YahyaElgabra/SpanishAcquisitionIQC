d8b04f957d198c2029ddc77e6b3281631bb41c5e
zmerino <zmerino2718@gmail.com>
2022-03-18 14:43:47 -0400

    copying code from dil-fridge computer

---
+++ spacq/devices/__init__.py
@@ -0,0 +1,2 @@
+from . import agilent, iqc, oxford, rohde_schwarz, tektronix, keithley, cryomagnetics, lakeshore, stanford_research_systems
+manufacturers = [agilent, iqc, oxford, rohde_schwarz, tektronix, keithley, cryomagnetics, lakeshore, stanford_research_systems]
